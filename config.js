06dc80eb88c9ab1f8e7fe20fd0ffc1180777f88e
patrickcmcp-pixel <patrickcmcp@gmail.com>
2026-03-12 16:13:30 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDxacZJIR4v0gNWw9UnfxQFcJlzW7wn4Dw",
+  authDomain: "sarahrenttracker.firebaseapp.com",
+  projectId: "sarahrenttracker",
+  storageBucket: "sarahrenttracker.firebasestorage.app",
+  messagingSenderId: "276065592139",
+  appId: "1:276065592139:web:be2ef046f3b32d87fc7ece"
+};
