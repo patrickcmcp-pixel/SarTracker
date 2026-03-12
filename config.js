eff03982b023790d701b7f102f219aa8ac8347c6
patrickcmcp-pixel <patrickcmcp@gmail.com>
2026-03-12 16:20:39 -0400

    config.js

--- config.js
+++ config.js
@@ -6,3 +6,6 @@ window.FIREBASE_CONFIG = {
   messagingSenderId: "276065592139",
   appId: "1:276065592139:web:be2ef046f3b32d87fc7ece"
 };
+
+// Set your 4-digit PIN here
+window.APP_PIN = "1994"; // <-- Change this to whatever you want
